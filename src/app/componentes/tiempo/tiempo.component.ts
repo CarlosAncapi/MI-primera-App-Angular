b4b577c0a8e47f9cf5eeca51d37709c06aa1af34
Carlos Ancapi <carlos.ancapi@gmail.com>
2023-03-18 06:12:04 -0300

    validators required formulario componente tiempo

--- src/app/componentes/tiempo/tiempo.component.ts
+++ src/app/componentes/tiempo/tiempo.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tiempo',
   templateUrl: './tiempo.component.html',
   styleUrls: ['./tiempo.component.css']
 })
-export class TiempoComponent {
+export class TiempoComponent implements OnInit {
 
+  formulario!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.iniciaFormulario();
+  }
+
+  ngOnInit(): void { 
+  }
+
+  /**
+   * Método que crea e inicia un formulario
+   */
+  iniciaFormulario(){
+    this.formulario = this.fb.group({
+      ciudad: ['', Validators.required  ],
+      codigo: ['', Validators.required  ]
+    })
+  }
+
+  consultar(){
+
+  }
 }
